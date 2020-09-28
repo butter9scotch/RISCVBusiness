0c4e5214ef37ddfbb7cd24950cef45105f41d5a4
Xinlue Liu <liu2141@purdue.edu>
2020-09-28 15:54:29 -0400

    fpu package decode&execute

--- source_code/packages/risc_mgmt/fpu_pkg.sv
+++ source_code/packages/risc_mgmt/fpu_pkg.sv
@@ -27,41 +27,45 @@
 
 package fpu_pkg;
 
-  localparam FPU_ADDSUBMUL = 7'b1010011; //op_code for add, sub, mul
-  localparam FPU_ADD = 7'b0000000;
-  localparam FPU_SUB = 7'b0000100;
-  localparam FPU_MUL = 7'b0001000;
-  localparam FPU_LD = 7'b0000111;
-  localparam FPU_SW = 7'b0100111;
+  localparam FPU_OPCODE_ARI = 7'b1010011; //opcode for arithmetic operation
+  localparam FPU_OPCODE_LD = 7'b0000111;  //opcode for load
+  localparam FPU_OPCODE_SW = 7'b0100111;  //opcode for store
+
+  //funct7(funct5 + fmt) for arithemtic operation
+  localparam FPU_FUNCT_ADD = 7'b0000000;  
+  localparam FPU_FUNCT_SUB = 7'b0000100;
+  localparam FPU_FUNCT_MUL = 7'b0001000;
 
   typedef struct packed {
-    logic [4:0] rs2;
-    logic [4:0] rs1;
-    logic [4:0] rd;
-    logic [2:0] frm_in;
-    logic [6:0] funct_7;
-    logic [31:0] dload_ext;
-    logic lw;
-    logic sw;
-  } fpu_insn_t;
+    logic [4:0] offset_funct5; //this may be imm[11:7] or funct5[4:0]
+    logic [1:0] offset_fmt;    //this may be imm[6:5] or fmt(.S will be 00)
+    logic [4:0] offset_rs2;    //this may be imm[4:0] or rs2
+    logic [4:0] rs1;           //register select 1
+    logic [2:0] width_rm;      //this may be width[2:0] or rm
+    logic [4:0] rd_offset;     //this may be imm[4:0] or rd
+    logic [6:0] opcode;        //opcode
+    logic [23:0] Reserved;     //reserved for other extension
+    logic [2:0] frm;           //rounding mode
+    logic [4:0] flags;         //flags(NV, DZ, OF, UF, NX)
+    } fpu_insn_t; //32 bit instruction and 32 bit fcsr
 
   // Interface between the decode and execute stage
   // This must be named "decode_execute_t"
   typedef struct packed {
-    logic start;
     logic add;
     logic sub;
     logic mul;
-    logic frm;
-    logic load;
-    logic store;
-  } decode_execute_t;
+    logic [4:0] rs1;
+    logic [4:0] rs2;
+    logic [4:0] rd;
+    logic [11:0] imm;
+  } decode_execute_t; //fcsr
 
   // Interface between the execute and memory stage
   // This must be named "execute_memory_t"
   typedef struct packed {
     logic signal;
-  } execute_memory_t;
+  } execute_memory_t; //doesn't output. Take leftover signals from execute. Load or store/bypass and go to memory . reg_w should be 0
 
   
 
