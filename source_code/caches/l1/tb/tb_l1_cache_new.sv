038b13df3124efe0fa2b0ba18767870043da3b9f
Raviteja Vajja <70241397+rvajja46@users.noreply.github.com>
2021-07-29 09:46:29 -0700

    added write miss test case

--- source_code/caches/l1/tb/tb_l1_cache_new.sv
+++ source_code/caches/l1/tb/tb_l1_cache_new.sv
@@ -35,19 +35,11 @@ module tb_l1_cache.sv
 	localparam CLK_PERIOD = 10;
 	localparam PROPAGATION_DELAY = 3;
 
-	// DUT Portmap Signals
-	logic tb_CLK;
-	logic tb_nRST;
-	logic tb_clear;
-	logic tb_flush;
-	logic tb_clear_done;
-	logic tb_flush_done;
-	generic_bus_if.cpu tb_mem_gen_bus_if;
-	generic_bus_if.generic_bus tb_proc_gen_bus_if;
-
 	// Testbench Signals
 	integer test_number;
 	string test_case;
+	logic tb_CLK;
+	logic tb_nRST;
 	typedef struct {
 		logic clear;
 		logic clear_done;
@@ -80,8 +72,8 @@ module tb_l1_cache.sv
 
 	// Task to simulate data write by CPU
 	task cpu_data_write;
-		input logic [RAM_WIDTH-1:0] data;
 		input logic [ADDR_WIDTH-1:0] addr;
+		input logic [RAM_WIDTH-1:0] data;
 	begin
 		@(posedge tb_CLK);
 		#(PROPAGATION_DELAY);
@@ -99,7 +91,6 @@ module tb_l1_cache.sv
 
 	// Task to simulate data read by CPU
 	task cpu_data_read;
-		input logic [RAM_WIDTH-1:0] data;  // Not necessary, can be used for checks
 		input logic [ADDR_WIDTH-1:0] addr;
 	begin
 		@(posedge tb_CLK);
@@ -116,8 +107,7 @@ module tb_l1_cache.sv
 
 
 	// Task to simulate instruction read by CPU
-	task cpu_data_read;
-		input logic [RAM_WIDTH-1:0] data;  // Not necessary, can be used for checks
+	task cpu_inst_read;
 		input logic [ADDR_WIDTH-1:0] addr;
 	begin
 		@(posedge tb_CLK);
@@ -143,8 +133,8 @@ module tb_l1_cache.sv
 	.clear_done(data_signals.clear_done),
 	.flush(data_signals.flush),
 	.flush_done(data_signals.flush_done),
-	.mem_gen_bus_if(data_signals.mem_gen_bus_if),
-	.proc_gen_bus_if(data_signals.proc_gen_bus_if));
+	.mem_gen_bus_if(data_signals.mem_gen_if),
+	.proc_gen_bus_if(data_signals.proc_gen_if));
 
 	// Instruction Cache Portmap
 	l1_cache #(.CACHE_SIZE(1024),
@@ -157,8 +147,8 @@ module tb_l1_cache.sv
 	.clear_done(inst_signals.clear_done),
 	.flush(inst_signals.flush),
 	.flush_done(inst_signals.flush_done),
-	.mem_gen_bus_if(inst_signals.mem_gen_bus_if),
-	.proc_gen_bus_if(inst_signals.proc_gen_bus_if));
+	.mem_gen_bus_if(inst_signals.mem_gen_if),
+	.proc_gen_bus_if(inst_signals.proc_gen_if));
 
 	// Clock generation block	
 	always begin
@@ -167,5 +157,67 @@ module tb_l1_cache.sv
     		tb_CLK = 1'b1;
     		#(CLK_PERIOD/2.0);
   	end
+
+	// Testbench Process
+	initial begin
+		test_number = 0;
+		test_case = "";
+
+		data_signals.clear = 1'b0;
+		data_signals.flush = 1'b0;
+		data_signals.mem_gen_if.busy = 1'b1;
+		data_signals.mem_gen_if.rdata = '0;
+		data_signals.proc_gen_if.ren = 1'b0;
+		data_signals.proc_gen_if.wen = 1'b0;
+		data_signals.proc_gen_if.wdata = '0;
+		data_signals.proc_gen_if.byte_en = 1'b0;
+
+		inst_signals.clear = 1'b0;
+		inst_signals.flush = 1'b0;
+		inst_signals.mem_gen_if.busy = 1'b1;
+		inst_signals.mem_gen_if.rdata = '0;
+		inst_signals.proc_gen_if.ren = 1'b0;
+		inst_signals.proc_gen_if.wen = 1'b0;
+		inst_signals.proc_gen_if.wdata = '0;
+		inst_signals.proc_gen_if.byte_en = 1'b0;
+
+		// TEST 00: Power-On and Reset Test
+		test_number = 0;
+		test_case = "Power-On and Reset";
+		
+		#(0.1);
+		tb_n_rst = 1'b0;
+
+		#(CLK_PERIOD * 0.5);
+
+		#(CLK_PERIOD);
+
+		@(posedge tb_clk);
+		#(PROPAGATION_DELAY * 3);
+		tb_n_rst = 1'b1;
+
+		// TEST 01: Write Miss to Data Cache
+		test_number++;
+		test_case = "Write Miss to Data Cache";
+
+		cpu_data_write('0, 32'DEAD_DEAD);
+		wait(data_signals.mem_gen_if.ren);
+		assert(data_signals.proc_gen_if.addr == data_signals.mem_gen_if.addr)
+		else $error("Test case: %s, test num: %0d, address from CPU does not match with incoming to main memory", test_case, test_num);
+
+		data_signals.mem_gen_if.busy = 1'b0;
+		data_signals.mem_gen_if.rdata = 32'hBEEF_BEEF;
+
+		@(posedge tb_CLK);
+		assert(data_signals.mem_gen_if.addr == data_signals.proc_gen_if.addr + 4)
+		else $error("Test case: %s, test num: %0d, second word address is wrong", test_case, test_num);
+
+		data_signals.mem_gen_if.rdata  = 32'hFFFF_AAAA;
+		wait(~data_signals.proc_gen_if.busy);
+		data_signals.mem_gen_if.busy  = 1'b1;
+		@(posedge CLK);
+		data_signals.proc_gen_if.wen  = 1'b0;
+
+	end
 
 endmodule
