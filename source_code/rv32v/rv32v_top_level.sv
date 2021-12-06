b045593aa95d915ecab4d2cdb2ea92cbe998acbc
ngildenhuys <nicholas@gildenhuys.net>
2021-12-05 22:40:49 -0500

    Added top level interface to test bench and modules

--- source_code/rv32v/rv32v_top_level.sv
+++ source_code/rv32v/rv32v_top_level.sv
@@ -4,14 +4,33 @@ module rv32v_top_level(
   cache_model_if.memory cif,
   rv32v_hazard_unit_if hu_if,
   prv_pipeline_if prv_if,
+  rv32v_top_level_if.dut top_if
+  /*
   output logic rd_wen,
   output logic [4:0] rd_sel,
   output logic [31:0] rd_data,
   input logic [31:0] xs1, xs2,
   input logic scalar_hazard_if_ret,
   input logic returnex
+  */
 );
 
+  // Outputs
+  logic rd_wen;
+  assign top_if.rd_wen = rd_wen;
+  logic [4:0] rd_sel;
+  assign top_if.rd_sel = rd_sel;
+  logic [31:0] rd_data;
+  assign top_if.rd_data = rd_data;
+
+  // Inputs
+  logic [31:0] xs1, xs2;
+  assign xs1 = top_if.xs1;
+  assign xs2 = top_if.xs2;
+  logic scalar_hazard_if_ret;
+  assign scalar_hazard_if_ret = top_if.scalar_hazard_if_ret;
+  logic returnex;
+  assign returnex = top_if.returnex;
 
 
 
