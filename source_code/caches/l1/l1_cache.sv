d59fa735a599c308f57eea13d175e2e486971e12
Mitch Arndt <mitchaarndt@gmail.com>
2022-03-31 18:51:21 -0400

    fixed bugs with parameterized next_read_addr

--- source_code/caches/l1/l1_cache.sv
+++ source_code/caches/l1/l1_cache.sv
@@ -357,12 +357,10 @@ module l1_cache #(
                     end 
                 end
 		        else if((proc_gen_bus_if.ren || proc_gen_bus_if.wen) && ~hit && ~cache[decoded_addr.set_bits].frames[ridx].dirty && ~pass_through) begin
-                	next_read_addr =  {decoded_addr.tag_bits, decoded_addr.set_bits, BLOCK_SIZE, 2'b00}; ////////////////////// FIX FOR WB to wrong address?
-			 end
-
-			else if((proc_gen_bus_if.ren || proc_gen_bus_if.wen) && ~hit && cache[decoded_addr.set_bits].frames[ridx].dirty && ~pass_through) begin
-			next_read_addr  =  {cache[decoded_addr.set_bits].frames[ridx].tag, decoded_addr.set_bits, BLOCK_SIZE, 2'b00};
-			   
+                	next_read_addr =  {decoded_addr.tag_bits, decoded_addr.set_bits, N_BLOCK_BITS'('0), 2'b00}; ////////////////////// FIX FOR WB to wrong address?
+			    end
+			    else if((proc_gen_bus_if.ren || proc_gen_bus_if.wen) && ~hit && cache[decoded_addr.set_bits].frames[ridx].dirty && ~pass_through) begin
+			        next_read_addr  =  {cache[decoded_addr.set_bits].frames[ridx].tag, decoded_addr.set_bits, N_BLOCK_BITS'('0), 2'b00};
             	end
             end // case: IDLE
 	    
@@ -392,7 +390,7 @@ module l1_cache #(
  
                 if(finish_word) begin
                     clr_word_ctr 					  = 1'b1;
-                    next_read_addr 					  = decoded_addr;
+                    next_read_addr  =  {decoded_addr.tag_bits, decoded_addr.set_bits, N_BLOCK_BITS'('0), 2'b00}; //TODO: CHECK THIS, ADDED BY VERIFICATION
                     next_cache[decoded_addr.set_bits].frames[ridx].dirty  = 1'b0;
                     mem_gen_bus_if.wen 					  = 1'b0;
                 end
