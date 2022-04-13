ea899ec59367b0c2f4aedf2024ed34ffe7d6fde3
Mitch Arndt <mitchaarndt@gmail.com>
2022-04-13 18:58:15 -0400

    updated run.py --help with more detailed information

--- source_code/caches/uvm-tb/run.py
+++ source_code/caches/uvm-tb/run.py
@@ -56,27 +56,48 @@ def seed_type(arg):
 
 def parse_arguments():
     global params
-    parser = argparse.ArgumentParser(description="Build and Run the UVM Testbench for the cache hierarchy")
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+                        description="\033[95m\033[1mBuild and Run the UVM Testbench for the cache hierarchy\n"
+                                    "  Note that all runtime parameters are saved in \033[4mrun_summary.log\033[0m")
     parser.add_argument('--clean', action="store_true",
                         help="Remove build artifacts")
     parser.add_argument('--build', action="store_true",
                         help="Build project without run")
     parser.add_argument('--testcase', '-t', type=str, default="random",
                         choices=["nominal", "evict", "index", "mmio", "random"],
-                        help="Specify name of the uvm test")
+                        help="Specify name of the uvm test:\n"
+                        "  nominal:   read back values previously written to caches\n"
+			            "  evict:     write to same index with different tag bits to force cache eviction\n"
+			            "  index:     read/write to same block of data to ensure proper block indexing\n"
+			            "  mmio:      read/write to memory mapped address space\n"
+			            "  random:    random interleaving of previous test cases")
     parser.add_argument('--gui', '-g', action='store_true',
                         help="Specify whether to run with gui or terminal only")
     parser.add_argument('--verbosity', '-v', type=str, default="low",
                         choices=["none", "low", "medium", "high", "full", "debug"],
-                        help="Specify the verbosity level to be used for UVM Logging")
+                        help="Specify the verbosity level to be used for UVM Logging, each stage builds on the next\n"
+                        "  none:  - only error messages shown\n"
+                        "  low:   - actual and expected values for scoreboard errors\n"
+                        "         - success msg for data matches\n"
+                        "         - sequence parameters\n"
+                        "  medium - actual and expected values for all scoreboard checks\n"
+                        "         - predictor default values for non-initialized memory\n"
+                        "         - end2end transaction/propagation details\n"
+                        "  high   - all uvm transactions detected in monitors, predictors, scoreboards\n"
+                        "         - predictor memory before:after\n"
+                        "  full   - all connections between analysis ports\n"
+                        "         - all agent sub-object instantiations\n"
+                        "         - all virtual interface accesses to uvm db\n"
+                        "  debug  - all messages")
     parser.add_argument('--seed', '-s', type=seed_type, default="random",
-                        help="Specify starter seed for uvm randomization")
+                        help="Specify starter seed for uvm randomization\n"
+                        "Identical seeds will produce identical runs")
     parser.add_argument('--iterations', '-i', type=int, default=0,
                         help="Specify the requested number of memory accesses for a test")
     parser.add_argument('--mem_timeout', type=int, default=50,
                         help="Specify the max memory latency before a fatal timeout error")
     parser.add_argument('--mem_latency', type=int, default=1,
-                        help="Specify the number of clock cycles before memory returns")
+                        help="Specify the number of clock cycles before main memory returns")
     parser.add_argument('--mmio_latency', type=int, default=2,
                         help="Specify the number of clock cycles before memory mapped IO returns")
     parser.add_argument('--config', type=str, default="full",
@@ -247,7 +268,7 @@ def post_run():
         except:
             cprint("{key:<15}-> {val}".format(key=key, val="None"), bcolors.FAIL)
     
-    with open("run_settings.log", "a") as out:
+    with open("run_summary.log", "a") as out:
         now = datetime.now()
         dt_string = now.strftime("%m-%d-%Y %H:%M:%S")
         
