43f65165f6f46dcba026c7c23dc8f60b66b68772
ngildenhuys <nicholas@gildenhuys.net>
2022-04-22 16:15:29 -0400

    fixed conflict

--- source_code/caches/separate_caches.sv
+++ source_code/caches/separate_caches.sv
@@ -1,12 +1,12 @@
 /*
 *   Copyright 2016 Purdue University
-*   
+*
 *   Licensed under the Apache License, Version 2.0 (the "License");
 *   you may not use this file except in compliance with the License.
 *   You may obtain a copy of the License at
-*   
+*
 *       http://www.apache.org/licenses/LICENSE-2.0
-*   
+*
 *   Unless required by applicable law or agreed to in writing, software
 *   distributed under the License is distributed on an "AS IS" BASIS,
 *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,7 @@
 *   Created by:   Jacob R. Stevens
 *   Email:        steven69@purdue.edu
 *   Date Created: 11/08/2016
-*   Description: Caches consisting of separate I$ and D$ 
+*   Description: Caches consisting of separate I$ and D$
 */
 
 `include "generic_bus_if.vh"
@@ -70,7 +70,7 @@ module separate_caches (
                           .clear_done(cc_if.dclear_done)
 			 );
 	 end
-      
+
     endcase
   endgenerate
 
