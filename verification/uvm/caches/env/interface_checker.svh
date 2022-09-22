14bea4ff2a5a41b5b3e668d1f55463b45defddeb
Mitch <arndt20@purdue.edu>
2022-09-22 14:31:18 -0400

    named assertions and added them to testplan

--- verification/uvm/caches/env/interface_checker.svh
+++ verification/uvm/caches/env/interface_checker.svh
@@ -38,25 +38,33 @@ module interface_checker (
 );
 
   // Flush done without a flush request
+  d_cif_flush_done :
   assert property (@(posedge d_cif.CLK) d_cif.flush_done |-> (d_cif.flush))
   else $fatal(1, "'d_cif.flush_done' should never be asserted without a cpu flush request");
+  i_cif_flush_done :
   assert property (@(posedge i_cif.CLK) i_cif.flush_done |-> (i_cif.flush))
   else $fatal(1, "'i_cif.flush_done' should never be asserted without a cpu flush request");
+  l2_cif_flush_done :
   assert property (@(posedge l2_cif.CLK) l2_cif.flush_done |-> (l2_cif.flush))
   else $fatal(1, "'l2_cif.flush_done' should never be asserted without a cpu flush request");
 
   // L2 Response without a read/write request
+  arb_l2_bus_if_busy :
   assert property (@(posedge l2_cif.CLK) !arb_l2_bus_if.busy |-> (arb_l2_bus_if.ren | arb_l2_bus_if.wen))
   else $fatal(1, "'arb_l2_bus_if.busy' should never be low without a cpu read/write request");
 
   // Ensure all bus transactions only occur when there is a read/write request
-  assert property (@(posedge d_cif.CLK) d_l1_arb_bus_if.ren |-> (d_cpu_if.ren))
+  d_l1_arb_bus_if_ren :
+  assert property (@(posedge d_cif.CLK) d_l1_arb_bus_if.ren |-> (!d_cpu_if.ren))
   else $fatal(1, "'d_l1_arb_bus_if.ren' should never be asserted without a cpu read request");
-  assert property (@(posedge d_cif.CLK) d_l1_arb_bus_if.wen |-> (d_cpu_if.wen))
+  d_l1_arb_bus_if_wen :
+  assert property (@(posedge d_cif.CLK) d_l1_arb_bus_if.wen |-> (!d_cpu_if.wen))
   else $fatal(1, "'d_l1_arb_bus_if.wen' should never be asserted without a cpu write request");
 
+  i_l1_arb_bus_if_ren :
   assert property (@(posedge d_cif.CLK) i_l1_arb_bus_if.ren |-> (i_cpu_if.ren))
   else $fatal(1, "'i_l1_arb_bus_if.ren' should never be asserted without a cpu read request");
+  i_l1_arb_bus_if_wen :
   assert property (@(posedge d_cif.CLK) i_l1_arb_bus_if.wen |-> (i_cpu_if.wen))
   else $fatal(1, "'i_l1_arb_bus_if.wen' should never be asserted without a cpu write request");
 
