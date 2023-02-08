42db7fad6ed755bfbaa6004af72393ccd3212358
Robert Murphy <murph417@purdue.edu>
2023-02-08 10:00:44 -0500

    Monitor structure for first two phases of trans complete

--- verification/uvm/bus_ctrl/bus_monitor.svh
+++ verification/uvm/bus_ctrl/bus_monitor.svh
@@ -4,172 +4,194 @@
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 `include "bus_ctrl_if.vh"
-/*class ahb_bus_monitor extends uvm_monitor;
+class bus_monitor extends uvm_monitor;
   `uvm_component_utils(ahb_bus_monitor)
 
-  virtual ahb_if vif;
+  localparam TRANS_SIZE = dut_params::WORD_W * dut_params::BLOCK_SIZE_WORDS;
 
-  uvm_analysis_port #(ahb_bus_transaction_v2) ahb_bus_ap;
-  uvm_analysis_port #(ahb_bus_transaction_v2) result_ap;
+  virtual bus_ctrl_if vif;
+
+  uvm_analysis_port #(bus_transaction) bus_ap;
+  uvm_analysis_port #(bus_transaction) result_ap;
   int timeoutCount;
 
   function new(string name, uvm_component parent = null);
     super.new(name, parent);
-    ahb_bus_ap = new("ahb_bus_ap", this);
+    bus_ap = new("bus_ap", this);
     result_ap  = new("result_ap", this);
   endfunction : new
 
   // Build Phase - Get handle to virtual if from config_db
   virtual function void build_phase(uvm_phase phase);
-    if (!uvm_config_db#(virtual ahb_if)::get(this, "", "ahb_vif", vif)) begin
+    if (!uvm_config_db#(virtual bus_ctrl_if)::get(this, "", "bus_ctrl_vif", vif)) begin
       `uvm_fatal("monitor", "No virtual interface specified for this monitor instance")
     end
   endfunction
 
+// txi is the index inside of the tx arrays we are checking with
+// vifi is the index in the virtual interface arrays we are checking with
+// This function compares the tx and the virtual interface to dertermine if the 
+//      transaction we have saved (at the txi index) matches the transaction on the interface (at vifi)
+//      returns 1 if they are different, 0 otherwise
+virtual function bit proc_req_different(bus_transaction tx, int txi, int vifi);
+   if(!tx.procReq[txi]) begin
+     return 1;
+   end else if(tx.dWEN[txi] != vif.dWEN[vifii] || tx.dREN[txi] != !vif.dWEN[vifi]
+                || tx.readX[txi] != vif.ccwrite[vifi] || tx.daddr[txi] != vif.daddr[vifi]
+                || tx.dstore[txi] != vif.dstore[vifi]) begin
+     return 1;
+   end
+
+   return 0;
+end
+
+endfunction
+
+
+virtual function bus_transaction zeroTrans(bus_transaction tx);
+  tx.idle = '0;
+  tx.daddr = '0;
+  tx.dWEN = '0;
+  tx.readX = '0;
+  tx.dstore = '0;
+  tx.dload = '0;
+  tx.exclusive = '0;
+  tx.snoopHitAddr = '0;
+  tx.snoopDirty = '0;
+  tx.numTransactions = 0;
+
+  tx.procReq = '0;
+  tx.snoopReq = '0;
+  tx.snoopRsp = '0;
+  tx.busCtrlRsp = '0;
+  tx.l2Req = '0;
+  tx.l2Rsp = '0;
+  tx.l2_rw = '0;
+  tx.procReqAddr = '0;
+  tx.l2ReqAddr = '0;
+  tx.snoopReqAddr = '0;
+  tx.snoopReqInvalidate = '0;
+  tx.snoopRspType = '0;
+  tx.procReqData = '0;
+  tx.snoopRspData = '0;
+  tx.l2RspData = '0;
+  tx.l2StoreData = '0;
+  tx.busCtrlRspData = '0;
+
+  return tx;
+endfunction
+  /* 1) check for processor requests and place the request in the next available slot for the given cpu
+     2) check for snoop requests, make sure that the snoop request to each cache is the same and place the general request into correct slot 
+        based on addr (which CPU slot basically)
+     3) check for snoop responses, make sure that if one says M then no others say M, if one says E then no others say E and depending on the
+        response place the response into the correct slot based on addr (which CPU's slot basically)
+     4) check for L2 requests by the bus_Ctrl and place into l2req location
+     5) Check for l2 responses and place into l2 resp location
+     6) Check for bus_ctrl rsp and place into the correct slot depending on the addresss. 
+     NOTE: If we can't find a matching address at a given point for a given phase then it's a fatal error
+     */
   virtual task run_phase(uvm_phase phase);
     super.run_phase(phase);
-    @(posedge vif.HCLK);
     forever begin
-      ahb_bus_transaction_v2 tx;
+      bus_transaction tx;
       timeoutCount = 0;
+      bit transComplete = 0;
+      bit snoopReqPhaseDone = 0;
+      bit snoopRspPhaseDone = 0;
+      bit l2ReqPhaseDone = 0;
+      bit l2RspPhaseDone = 0;
+      int cpuIndx[dut_params::NUM_CPUS_USED-1:0];
+      int nonSnoopProc = 0; // this is the processor that should be recieving the data at the end!
+
       // captures activity between the driver and DUT
-      tx = ahb_bus_transaction_v2::type_id::create("tx");
+      tx = bus_transaction::type_id::create("tx");
 
       // zero out everything
-      tx.haddr = '0;
-      tx.hwdata = '0;
-      tx.hwstrb = '0;
-      tx.hrdata_out = '0;
-      for (int i = 0; i < 126; i++) begin
-        tx.numErrors[i] = 0;
-      end
-      for (int i = 0; i < 126; i++) begin
-        tx.timeout_amount[i] = 0;
-      end
-      tx.hready_timeout = '0;
+      tx = zeroTrans(tx);
 
-      tx.haddr[0] = vif.HADDR;
-      //tx.hwdata[0] = vif.HWDATA;
-      //tx.hwstrb[0] = vif.HWSTRB;
-      tx.rw = vif.HWRITE;
-      tx.idle = vif.HTRANS == 2'b0;
+      `zero_unpckd_array(cpuIndx);
+      
+      while(!transComplete) begin
+        @(posedge vif.clk);
+        
+        // Check for new processor requests
+        // Throw error if we have write & read or write & ccwrite OR if a new request starts before the old one ends
+        if(|vif.dREN || |vif.dWEN) begin
+           for(int i = 0; i < dut_params::NUM_CPUS_USED; i++) begin
+             if((vif.dREN[i] && vif.dWEN[i]) || (vif.dWEN[i] && vif.ccwrite[i])) begin
+                `uvm_fatal("Monitor", $sformatf("Invalid combination of CPUS requests %d", i));
+             end else if(vif.dREN[i] && proc_req_different(tx, cpuIndx[i], i)) begin
+                if(tx.procReq[cpuIndx[i]]) begin
+                  `uvm_fatal("Monitor", $sformatf("req not complete before new request! proc %d", i));
+                end
 
-      case (vif.HBURST)
-        0: tx.burstType = 3;  // single transfer
-        1: tx.burstType = 2;  // undefined length incr
-        2: tx.burstType = 0;  // wrap
-        3: tx.burstType = 1;  // incr
-        4: tx.burstType = 0;  // wrap
-        5: tx.burstType = 1;  // incr
-        6: tx.burstType = 0;  // wrap
-        7: tx.burstType = 1;  // incr
-      endcase
+                tx.procReq[cpuIndx[i]] = 1;
+                tx.daddr[cpuIndx[i]] = vif.daddr[i];
+                tx.dWEN[i] = 0;
+                tx.readX[i] = vif.ccwrite;
+             end else if(vif.dWEN[i] && proc_req_different(tx, cpuIndx[i], i)) begin
+                if(tx.procReq[cpuIndx[i]]) begin
+                  `uvm_fatal("Monitor", $sformatf("req not complete before new request! proc %d", i));
+                end
 
-      case (vif.HBURST)
-        2: tx.burstSize = 0;  // size 4
-        3: tx.burstSize = 0;
-        4: tx.burstSize = 1;  // size 8
-        5: tx.burstSize = 1;
-        6: tx.burstSize = 2;  // size 16
-        7: tx.burstSize = 2;
-        default: tx.burstSize = 0;
-      endcase
-
-      tx.hsize = vif.HSIZE;
-      tx.hsel  = vif.HSEL;
-
-      // check if there is a new input to the subordinate
-      if (vif.HSEL == 1'b1) begin
-        // send the new input data to predictor though ahb_bus_ap
-
-        //`uvm_info(this.get_name(), "New item sent to predictor", UVM_LOW);
-        // depending on what type of transaction
-        case (vif.HTRANS)
-          0: begin  // if it is an idle transfer
-            @(posedge vif.HCLK);
-            tx.hwdata[0] = vif.HWDATA;
-            tx.hwstrb[0] = vif.HWSTRB;
-            // We should see a zero wait state okay response
-            while (!vif.HREADYOUT && timeoutCount != 1 && !vif.HRESP) begin
-              timeoutCount = timeoutCount + 1;
-              @(posedge vif.HCLK);
+                tx.procReq[cpuIndex[i]] = 1;
+                tx.dWEN[cpuIndx[i]] = 1;
+                tx.readX[cpuIndx[i]] = 0;
+                tx.daddr[cpuIndx[i]] = vif.daddr[i];
             end
-            //tx.hrdata_out[0]  = vif.HRDATA; don't worry about data, master won't be reading it during idle anyways
-            //tx.hresp_out[0][0] = vif.HRESP;
-            tx.numErrors[0]   = vif.HRESP;  // if there is an error this will count one
-            tx.hready_timeout = timeoutCount == 1;
-            tx.timeout_amount = timeoutCount;
-          end
-          2: begin  // NONSEQ transfers
-            if (vif.HBURST == '0) begin  // single transfer
-              @(posedge vif.HCLK);
-              tx.hwdata[0] = vif.HWDATA;
-              tx.hwstrb[0] = vif.HWSTRB;
-              while (!vif.HREADYOUT && timeoutCount != 100 && !vif.HRESP) begin
-                timeoutCount = timeoutCount + 1;
-                @(posedge vif.HCLK);
-              end
-              tx.timeout_amount = timeoutCount;
-              tx.hready_timeout = timeoutCount == 100;
-              if (!tx.rw) begin  // if we're not writing, we care about the hrdata outptu
-                tx.hrdata_out[0] = vif.HRDATA;
-              end
-              //tx.hresp_out[0][0] = vif.HRESP;
-              tx.numErrors[0] = 0;
-              while (vif.HRESP && timeoutCount == 0) begin  // if we see an error state we should count them all
-                tx.numErrors[0] = tx.numErrors[0] + 1;
-                @(posedge vif.HCLK);
-              end
-            end else begin  // All other burst types look the same basically
-              tx.burstLength = 1;
-              for (int i = 0; i < 128; i++) begin  // the max length of undefined burst is 128
-                //`uvm_info(this.get_name(), "Monitor burst", UVM_LOW);
-                timeoutCount = 0;
-                @(posedge vif.HCLK);
-                tx.hwdata[i] = vif.HWDATA;
-                tx.hwstrb[i] = vif.HWSTRB;
-                while (!vif.HREADYOUT && timeoutCount != 100 && !vif.HRESP) begin
-                  timeoutCount = timeoutCount + 1;
-                  @(posedge vif.HCLK);
-                end
-                tx.timeout_amount[i] = timeoutCount;
+           end
+        end
 
-                tx.hready_timeout = timeoutCount == 100;
-                if (!tx.rw) begin  // if we are writing, we don't care about hrdata output
-                  tx.hrdata_out[i] = vif.HRDATA;
-                end
-                //tx.hresp_out[0][0] = vif.HRESP;
-                tx.numErrors[i] = 0;
-                while (vif.HRESP && timeoutCount == 0) begin  // if we see an error state we should count them all
-                  tx.numErrors[i] = tx.numErrors[i] + 1;
-                  @(posedge vif.HCLK);
-                end
+        // Check for new snoop requests
+        if(|vif.ccwait && !snoopReqPhaseDone) begin
+           for(int i = 0; i < dut_params::NUM_CPUS_USED; i++) begin
+             // Figure out if this is the processor that's going to eventually recieve the data
+             // If we have multiple being not snooped then this is a problem!!
+             if(!vif.ccwait[i] && (nonSnoopProc == -1) && !snoopReqPhaseDone) begin
+               nonSnoopProc = i;
+             end else if(!vif.ccwait[i] && (nonSnoopProc != -1) && !snoopReqPhaseDone) begin
+               `uvm_fatal("Monitor", $sformatf("Multiple processors not being snooped when one is!"));
+             end
+           end
 
-                if(vif.HTRANS == 2'b0 || vif.HTRANS == 2'b10 || i == 127) begin // break out if this is the end of the transfer
-                  break;
-                end
-                // Set the next transactions signals
-                tx.haddr[i+1]  = vif.HADDR;
-                //tx.hwdata[i+1] = vif.HWDATA;
-                //tx.hwstrb[i+1] = vif.HWSTRB;
-                tx.burstLength = tx.burstLength + 1;
-              end
-            end
-          end
-        endcase
+           // make sure that if there's an invalidate signal that all of them are set except for the req being handled
+           if(|vif.ccinv && !(&(vif.ccinv || (1'b1 << nonSnoopProc)))) begin
+             `uvm_fatal("Monitor", "All snoops not recieving same invalidate signal!");
+           end
 
-        ahb_bus_ap.write(tx);
+           // Make sure that the snoop address is the same for all of them
+           for(int i = 0; i < dut_params::NUM_CPUS_USED; i++) begin
+             if((i != nonSnoopProc) && (vif.ccsnoopaddr[i] != tx.daddr[nonSnoopProc])) begin
+                `uvm_fatal("Monitor", $sformatf("The snoop address does not match the processor request address, snooping into cpu #%d", i));
+             end
+           end
 
-        // now write the result to the scoreboard!
-        //`uvm_info(this.get_name(), "New result sent to scoreboard", UVM_LOW);
-        result_ap.write(tx);
-      end else begin
-        @(posedge vif.HCLK);
+           // Now set the signals now that we've checked all of the conditions
+           tx.snoopReq[nonSnoopProc] = 1;
+           snoopReqAddr[nonSnoopProc] = tx.daddr[nonSnoopProc];
+           snoopReqInvalidate[nonSnoopProc] = |vif.ccinv;
+           snoopReqPhaseDone = 1;
+           end
+        end
+
+       // Check for new snoop responses 
+
+        // Remember to set the nonSnoop processor to zero at the end
+        // Also remember to update the cpuIndx(s) once the rsp is recieved from the bus controller!
+        // Also remember to set the done flags to zero after the procRsp phase is complete
+
       end
+
+
+    bus_ap.write(tx);
+
+    // now write the result to the scoreboard!
+    `uvm_info(this.get_name(), "New result sent to scoreboard", UVM_LOW);
+    result_ap.write(tx);
 
     end
   endtask : run_phase
 
-endclass : ahb_bus_monitor*/
+endclass : bus_monitor
 
 `endif
